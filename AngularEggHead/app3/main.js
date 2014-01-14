752c6142dc993d5163510c6034a6956833ff476b
Daniel Mackay <crankage@gmail.com>
2014-01-14 19:12:47 +1100

    Directive

---
+++ AngularEggHead/app3/main.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="../scripts/angular.min.js" />
+var app = angular.module("superhero", []);
+
+app.directive("superman", function () {
+    return {
+        restrict: "E",
+        template: "<div>Here I am to save the day</div>"
+    };
+})
