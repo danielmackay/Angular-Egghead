3d932513c26adbce3f320ea629e1550a839eeaad
Daniel Mackay <crankage@gmail.com>
2014-01-18 15:43:26 +1100

    isolate scope '&'

---
+++ AngularEggHead/app11-isolate-scope-ampersand/main.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module("phoneApp", []);
+
+app.controller("AppCtrl", function($scope) {
+    $scope.callHome = function(message) {
+        alert(message);
+    };
+});
+app.directive("phone", function() {
+    return {
+        scope: {
+            dial: "&",
+        },
+        template: '<input class="form-control" type="text" ng-model="value">' +
+            '<div class="btn btn-primary" ng-click="dial({message:value})">Call home!</div>'
+    };
+});
