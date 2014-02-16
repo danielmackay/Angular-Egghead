a9d3a17d2c6af1238cdcefc0d16133c493f7c2f9
Daniel Mackay <crankage@gmail.com>
2014-02-16 18:15:12 +1100

    Promises

---
+++ AngularEggHead/app24-promises/main.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module("app", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+      .when('/',
+      {
+        templateUrl: "app.html",
+        controller: "AppCtrl"
+      });
+});
+
+app.controller("AppCtrl", function ($scope, $q) {
+  var defer = $q.defer();
+
+  defer.promise
+    .then(function (weapon) {
+      alert('you can have my ' + weapon);
+    return "bow";
+  }).then(function (weapon) {
+    alert('and my ' + weapon);
+    return "Axe";
+  }).then(function (weapon) {
+    alert('and my ' + weapon);
+    });
+
+  defer.resolve("sword");
+
+  $scope.model = {
+    message:  "This is my app!"
+  };
+});
